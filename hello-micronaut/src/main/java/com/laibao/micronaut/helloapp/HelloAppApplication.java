159f951ddf40ff00380e2ac9436b11cb6e72927c
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-10-19 08:54:03 +0800

    update pom.xml file info

---
+++ hello-micronaut/src/main/java/com/laibao/micronaut/helloapp/HelloAppApplication.java
@@ -0,0 +1,10 @@
+package com.laibao.micronaut.helloapp;
+
+import io.micronaut.runtime.Micronaut;
+
+public class HelloAppApplication {
+
+    public static void main(String[] args) {
+        Micronaut.run(HelloAppApplication.class);
+    }
+}
